3fa29260d8865bccd3c0fe1a904b503f4663ce1f
SiarheiSychou <siarheisychou1999@gmail.com>
2022-11-28 14:33:27 +0400

    implemented unique

---
+++ script.js
@@ -0,0 +1,12 @@
+var uniqueInOrder = function (iterable) {
+  let arr = [];
+   for (let i = 0; i < iterable.length; i++) {
+   if (iterable[i] !== iterable[i+1]) {
+     arr.push(iterable[i]);
+   }
+ }
+ return arr;
+}
+
+let result = uniqueInOrder('AAAAAAABBBBBbbbCCCCdddddddddDDDDDeeeEEEEEEEEEE');
+console.log(result);
